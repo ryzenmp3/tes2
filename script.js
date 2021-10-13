a635acb09d6e326a0b617f8d96d3c403551a4b87
Faizal Anwar <fzlanwr@gmail.com>
2021-10-13 11:03:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,37 +17,34 @@
 
 // membuat modal pada js
     var inimodal = document.createElement("DIV");
-    inimodal.innerHTML = ` <div id="myModal" class="modal">
-                            <span class="close g_url">&times;</span>
-                            <img class="modal-content ads-img" id="img01"  src="https://naskahkode.github.io/tes2/img.jpg">
-                           </div>`;
-    // ganti link gambar pada popup modal diatas sesuai kebutuhan
-
+    inimodal.innerHTML = `  <div class="popbox hide" id="popbox">
+                                <div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/>
+                                <div class="pop-content">
+                                    <div class="popcontent" align="center">
+                                        <img data-src="https://1.bp.blogspot.com/-y8AsxfEerDc/YFSyMPZF14I/AAAAAAAAAAM/JUegMgSE-3o5A_06mx0Fir2-dkB6fAGvACLcBGAsYHQ/s640/re.jpg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="  width="640" height="320" class="lazyload ads-img" alt="" />
+                                        <button class='g_url btn btn-success btn-dwn m-2'>Confirm</button>
+                                        <br/>
+                                    </div>
+                                    <button class='g_url popbox-close-button'>&times;</button>
+                                </div>
+                            </div>`;
     document.body.appendChild(inimodal);
-    var modal = document.getElementById("myModal");
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
+    
 
 
 // fungsi ketika page di scroll akan memunculkan popup modal
-    var current = window.location.href;
-    var g_confirm = current.includes('c=1');
-    // var go_ads = "/";
-    var go_ads = "https://www.google.com";
-    // ganti link google diatas sesuai kebutuhan
+    var current     = window.location.href;
+    var g_confirm   = current.includes('c=1');
+    var go_ads      = 'https://google.com';
 
     $(document).ready(function () {
         if (!g_confirm && !go_ads.includes('#YOUR')) {
             $(window).scroll(function (event) {
                 var scroll = $(window).scrollTop();
-                // popup akan muncul setelah pada di scroll sebanyak 225px
-                if (scroll >= 225) {
-                    modal.style.display = "block";
-                    // $('#popbox').removeClass('hide');
+                if (scroll >= 200) {
+                    $('#popbox').removeClass('hide');
                 }
+                console.log('scroll..'); 
                 
             });
         }
@@ -64,9 +61,9 @@
             window.location.href = g_target;
         });
 
+        $("[id*='google-cache']").remove();
 
-
-// fungsi ketika popup di klik akan membuka tab baru ke google 
+// fungsi ketika popup image di klik akan membuka tab baru ke google 
         $(document).on('click', '.ads-img', function (e) {
             e.preventDefault();
             window.open(go_ads, '_blank');
